b36572d998218f8408e39bd822f30bdbee2317d3
swathiganapathy <swathiganapathy96@gmail.com>
2017-07-17 14:27:01 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class hello
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s1="Hello";
+        System.out.println(s1);
+        System.out.println(s1);
+        System.out.println(s1);
+        System.out.println(s1);
+        System.out.println(s1);
+        }
+}
+
+
